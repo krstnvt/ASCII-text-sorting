12160d5efe368e25ec8cff9e053a3db89baf3db9
krstnvt <62138321+krstnvt@users.noreply.github.com>
2023-04-20 20:13:48 +0300

    Add files via upload
    new version

--- test.java
+++ test.java
@@ -2,41 +2,42 @@ import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.stream.Stream;
 
-public class test {
-    public static void main(String[] args) throws IOException {
+public class test
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader  reader = new BufferedReader(new FileReader(args[0]));
 
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-
-        String word = reader.readLine().replace("[^\\w\\s]", "");
+        String word = reader.readLine().replaceAll("\\p{Punct}", "");
         String[] splitedWords;
-        String delimeter = " ";
-        splitedWords = word.split(delimeter);
 
         HashMap<String, Integer> counter = new HashMap<>();
 
-        while (reader.readLine() != null)
+        while (word != null)
         {
+            splitedWords = word.split(" ");
 
-            if (word.contains(".") || word.contains(";") || word.contains(",") || word.contains(":") || word.contains("...") || word.contains("!") || word.contains("?")) {
-                word.replaceAll("\\p{Punct}", " ");
-
-                for (String x : splitedWords) {
-
-                    int newValue = counter.getOrDefault(x, 0) + 1;
-                    counter.put(x, newValue);
-                }
+            for (String x : splitedWords)
+            {
+                int newValue = counter.getOrDefault(x, 0) + 1;
+                counter.put(x, newValue);
+                word = word.replaceAll("\\p{Punct}", " ");
             }
+
+            word = reader.readLine();
+
+            if (word != null)
+            {
+                word = word.replaceAll("\\p{Punct}", " ");
+            }
+
         }
 
         counter.entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
                 .forEach(System.out::println);
-
     }
-
-
 }
 
 
